41ce0aa8704f758f7fa258c0bc5b6db9c90f41c1
Karima Jaman Moon <karimajaman9876@gmail.com>
2022-09-03 12:47:49 +0600

    multiple_authentication

---
+++ resources/views/backend/admin/admin_login.blade.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Admin Login</title>
+  </head>
+  <body>
+              <div class="container">
+                <div class="row">
+                    <div class="col-md-3"></div>
+                      <div class="col-md-6">
+                              <form action="#" class="mt-5" method="POST">
+                                 @csrf
+                              <div class="form-group">
+                                  <label>Email</label>
+                                  <input type="email" name="email" class="form-control">
+                              </div>
+                              <div class="form-group">
+                                  <label>Password</label>
+                                  <input type="password" name="pass" class="form-control">
+                              </div>
+                              <div class="form-group">
+                                <input type="submit" value="Admin Login" class="btn btn-success mt-3">
+                              </div>    
+                            </form>
+                        </div>
+                    </div>
+                 </div>
+              </div>
+
+                  
+                  
+
+             
+  
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  </body>
+</html>
